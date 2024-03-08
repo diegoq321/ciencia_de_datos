747c11fc0b7b87262d781ce5dfcbbf62074efe12
diegoq321 <diegoq321@hotmail.com>
2024-03-08 18:50:02 -0500

    Inicio del repositorio de portafolio

---
+++ R/titanic.R
@@ -0,0 +1,13 @@
+#===========================#
+# Titanic survival analisys #
+#  Author: Diego Quiguango  #
+#===========================#
+
+
+# Importacion de librerias
+if(!require('mlbench')) install.packages('mlbench')
+if(!require('ggplot2')) install.packages('ggplot2')
+if(!require('dplyr')) install.packages('dplyr')
+if(!require('caret')) install.packages('caret')
+if(!require('catools')) install.packages('catools')
+
